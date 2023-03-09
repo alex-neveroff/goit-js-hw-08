00cd9970fe409027c90074e0366018febee767f7
Alexandr Neveroff <neveroff1976@gmail.com>
2023-03-09 17:41:45 +0200

    task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(updateTime, 1000));
+player.setCurrentTime(timeFromLocalStorage()).catch(function (error) {
+  console.error(error);
+});
+
+function updateTime() {
+  player.getCurrentTime().then(function (seconds) {
+    timeToLocalStorage(seconds);
+  });
+}
+
+function timeToLocalStorage(time) {
+  localStorage.setItem('videoplayer-current-time', time);
+}
+
+function timeFromLocalStorage() {
+  if (localStorage.getItem('videoplayer-current-time')) {
+    return localStorage.getItem('videoplayer-current-time');
+  }
+  return 0;
+}
